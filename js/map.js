8dc8326dd3884d04500bf73ceadc623ac774bc8f
aniestkh <108396590+aniestkh@users.noreply.github.com>
2022-10-22 02:02:42 +0900

    Add files via upload

--- js/map.js
+++ js/map.js
@@ -1,8 +1,10 @@
 
 $('.close').on('click', function(){
     $('.membership').removeClass('on')
+    $('.headmenu').removeClass('on')
 })
 
 $('.resize_pack').on('click', function(){
     $('.membership').addClass('on')
+    $('.headmenu').addClass('on')
 })
